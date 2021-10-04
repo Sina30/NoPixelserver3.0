3c4faf825bb4d980bff9a8ff10d39f060484d4ba
Sina30 <santiano38@googlemail.com>
2021-10-04 12:18:28 +0200

    MAP

---
+++ resources/[MAP]/nutt_integrity_lobby/(README)IMPORTANT.txt
@@ -0,0 +1,7 @@
+ 
+ 
+If you need a working version of integrity with pillbox v1 or v2
+you need to join the discord and make a patreon support ticket. 
+
+
+Honeynutt's Discord Server: https://discord.gg/7KX35Zp
---
+++ resources/[MAP]/nutt_integrity_lobby/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+description 'Integrity'
+made_by 'Honeynutt, Syncedgamingtv, Dolutattoo'
+
+this_is_a_map 'yes'
+
+
+
+data_file 'TIMECYCLEMOD_FILE' 'nutt_timecycle_mods_1.xml'
+
+
+files {
+         "nutt_timecycle_mods_1.xml",
+
+}
+
